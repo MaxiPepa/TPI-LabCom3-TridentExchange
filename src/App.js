d28a962fd56be17dc433818a5b3d3f1fd235059f
MaxiPepa <maximopepa@gmail.com>
2022-09-19 19:26:03 -0300

    Created RegisterForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
 
 import NavBar from "./components/Header/NavBar/NavBar";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="main-program">
       <NavBar />
+      <RegisterForm />
       <Footer />
     </div>
   );
