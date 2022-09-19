b168f80dc9c505ac911e0cee586277bdaa3069f2
Bortoli94 <gbortoli.rosario@hotmail.com>
2022-09-19 19:38:03 -0300

    loginDecorationV1

--- src/components/Header/SingIn/SignIn.js
+++ src/components/Header/SingIn/SignIn.js
@@ -3,10 +3,14 @@ import "./SignIn.css";
 const SignIn = () => {
   return (
     <div className="sign-in">
-      <label for="user">Usuario: </label>
-      <input type="text" />
-      <label for="password">Contraseña: </label>
-      <input type="text" />
+      <label for="user">Usuario:</label>
+      <input type="text" id="user"/>
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password"/>
+      <div className="button_sign-in">
+        <button type="button">Iniciar Sesión</button>
+        <button type="button">Registrase</button>
+      </div>
     </div>
   );
 };
