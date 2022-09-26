82207bad14f8ea87aaa5bf3eff858621d793ec28
MaxiPepa <maximopepa@gmail.com>
2022-09-26 19:18:02 -0300

    Added and refactor to use React Router

--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -1,14 +1,10 @@
 import "./NavBar.css";
 
-import SignIn from "../SingIn/SignIn";
-
 const NavBar = () => {
   return (
     <header>
       <nav>
         <h1>Trident Exchanges</h1>
-
-        <SignIn />
       </nav>
     </header>
   );
