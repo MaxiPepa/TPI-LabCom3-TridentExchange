db64bd560b2eac90647de3061ded63d0f825ac17
MaxiPepa <maximopepa@gmail.com>
2022-09-26 19:50:25 -0300

    Fixed footer to be on the bottom

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
+
+import "./Layout.css"
+
 import Footer from "../Footer/Footer";
 import NavBar from "../Header/NavBar/NavBar";
 
 const Layout = () => {
   return (
-    <>
+    <div className="main-content">
       <NavBar />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
