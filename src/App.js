58af14d64610ce5979168e2fe59627504b7102c9
MaxiPepa <maximopepa@gmail.com>
2022-10-02 15:13:00 -0300

    Added Category Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import "./App.css";
 
+import CategoryPage from "./components/CategoryPage/CategoryPage";
 import Layout from "./components/Layout/Layout";
 import NewUser from "./components/RegisterForm/NewUser/NewUser";
 
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<Navigate replace to="/login" />} />
       <Route path="/login" element={<NewUser />} />
+      <Route path="/categories" element={<CategoryPage />} />
     </Route>
   )
 );
---
+++ src/components/CategoryPage/CategoryPage.js
@@ -0,0 +1,38 @@
+import "./CategoryPage.css"
+
+const CategoryPage = () => {
+  return (
+    <div className="category-container">
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Electrodomésticos</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Muebles</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Cocina</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Electrónica</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Patio</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Juguetería</h1>
+      </button>
+      <button type="button" className="category-item">
+        <img alt=""/>
+        <h1>Electrodomésticos</h1>
+      </button>
+    </div>
+  );
+};
+
+export default CategoryPage;
