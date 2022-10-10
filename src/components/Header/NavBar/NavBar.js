f49c2097c49bce0afee141ec0f020ca05f173831
MaxiPepa <maximopepa@gmail.com>
2022-10-10 12:24:03 -0300

    Added the posibility to see who is conected in the navbar

--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -1,11 +1,28 @@
 import "./NavBar.css";
 
+import { useAuth } from "../../Contexts/AuthContext";
 
 const NavBar = () => {
+  const { userInfo, logout } = useAuth();
+
+  const logOutButtonHandler = async () => {
+    await logout();
+  };
+
   return (
     <header>
       <nav>
         <img src="/img/trident-exchange-logo.png" alt="Trident Exchange Logo" />
+
+        {userInfo != null && (
+          <div className="singed-in-user">
+            <p>Bienvenido, {userInfo.email}</p>
+            <button type="button">Configuración</button>
+            <button type="button" onClick={logOutButtonHandler}>
+              Desconectarse
+            </button>
+          </div>
+        )}
       </nav>
     </header>
   );
