ce9be6f1b8f6e954061f09ddf71e2a45f4f1f9e6
MaxiPepa <maximopepa@gmail.com>
2022-10-10 21:31:23 -0300

    Added css and form

--- src/components/NewOffer/NewOffer.js
+++ src/components/NewOffer/NewOffer.js
@@ -1,4 +1,4 @@
-import "./NewOffer.css"
+import "./NewOffer.css";
 
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -14,7 +14,21 @@ const NewOffer = () => {
     }
   }, [userInfo, navigate]);
 
-  return <div>NewOffer</div>;
+  return (
+    <form className="new-offer-form">
+      <label htmlFor="title">Título:</label>
+      <input id="title" type="text" />
+      <label htmlFor="description">Description:</label>
+      <textarea id="description" rows="15"/>
+      <label htmlFor="photo">Link de la foto:</label>
+      <input id="photo" type="text" />
+      <label htmlFor="item">Item de preferencia:</label>
+      <input id="item" type="text" />
+      <label htmlFor="contact">Número o Email de contacto:</label>
+      <input id="contact" type="text" />
+      <button type="button">Ingresar oferta</button>
+    </form>
+  );
 };
 
 export default NewOffer;
